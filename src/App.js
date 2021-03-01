c385359a21b630a095b3796e51fbbdd3cf7b0578
Rithik <duttrithik@gmail.com>
2021-03-02 02:17:25 +0530

    Login update
    Signed-off-by: Rithik <duttrithik@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import SignUp from './Components/Login/signup';
 function App() {
   return (
     <div className="App">
-      <h1>
-        MeTube
-      </h1>
+      
       <Route path = '/login' component = {Login} />
       <Route path = '/signup' component = {SignUp} />
     </div>
--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,87 +1,76 @@
-import React, {Component} from 'react';
+import React,{useState} from 'react';
 import './login.css';
-import { Button, Card, CardBody, Form, FormGroup, Col, Input } from 'reactstrap';
+import { Card, CardBody} from 'reactstrap';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-class Login extends Component {
+function Login(){
+  const [Email,setEmail] = useState('');
+  const [Password, setPassword] = useState('');
+  const [EmailError,setEmailError] = useState(false);
+  const [PasswordError,setPasswordError] = useState(false);
+  const [EmailErrorMsg,setEmailErrorMsg] = useState('');
+  const [PasswordErrorMsg,setPasswordErrorMsg] = useState('');
 
-  state = { 
-    email: "",
-    password: ""
-}
 
-handleChange(event, element) {
-  var value = event.currentTarget.value
-  if(element === "email") {
-      this.setState({
-          email: value
-      })
+  const handleChange = (event, element) =>{
+    var value = event.currentTarget.value
+    if(element === "email") {
+        setEmail(value);
+    }
+    else if(element === "password") {
+        setPassword(value);
+    }
   }
-  else if(element === "password") {
-      this.setState({
-          password: value
-      })
+
+  const handleLogin = ()=> {
+    console.log(Email);
+    console.log(Password);
   }
-}
-
-handleLogin() {
-  console.log(this.state.email);
-  console.log(this.state.password);
-}
-
-
-  render() {
-      return (
-        <div className="container">
+  return (
+    <div className="login">
       <div className='row'>
         <div className='col-12 col-md-6 offset-md-3'>
-          <Card className='login-card'>
+          <Card className='login-card mt-4'>
             <CardBody>
               <div className='container'>
-                <div className='row'>
+                <div style = {{textAlign:'center'}}>
                   <img src='https://blog.logomyway.com/wp-content/uploads/2020/09/youtube-logo-4.jpg' alt='YT_Logo' className='YT_Logo'></img>
                 </div>
                 <div className='login-text'>
-                  <div className='row mt-4'>
+                  <div className='mt-4' style = {{textAlign:'center'}}>
                     <h2>
                       <b>Log in</b>
                     </h2>
                   </div>
-                  <div className='row'>
+                  <div className='' style = {{textAlign:'center'}}>
                     <h6>
-                      <b>to coninue to our website</b>
+                      <b>to continue to our website</b>
                     </h6>
                   </div>
                 </div>
-                <div className='row mt-4'>
+                <div className='mt-4 textfield'>
                   <div className='col-12'>
-                    <Form>
-                      <FormGroup row className='mt-2'>
-                        <Col>
-                          <Input type="email" name="email" placeholder="Email-ID" onChange={(event) => this.handleChange(event, "email")}/>
-                        </Col>
-                      </FormGroup>
-                      <FormGroup row className='mt-2'>
-                        <Col>
-                          <Input type="password" name="password" placeholder="Password" onChange={(event) => this.handleChange(event, "password")}/>
-                        </Col>
-                      </FormGroup>
-                    </Form>
+                    {EmailError?<TextField error id="outlined-basic" label="Email" variant="outlined" onChange={(event) => handleChange(event, "email")} style = {{width : '100%'}} helperText = {EmailErrorMsg}/>:<TextField id="outlined-basic" label="Email" variant="outlined" onChange={(event) => handleChange(event, "email")} style = {{width : '100%'}}/>}
+                    <div className ='mt-3'>{PasswordError?<TextField error id="outlined-basic" label="Password" variant="outlined" type = 'password' onChange={(event) => handleChange(event, "password")} style = {{width : '100%'}} helperText = {PasswordErrorMsg}/>:<TextField id="outlined-basic" label="Password" variant="outlined" onChange={(event) => handleChange(event, "password")} style = {{width : '100%'}}/>}</div>
                   </div>
                 </div>
                 <div className='login-text'>
-                  <div className='row mt-4'>
-                    <a href='#' className='login_links'>
+                  <div className='mt-4' style = {{textAlign:'left'}}>
+                    <a href='/login' className='login_links col-12 '>
                       Forgot Password?
                     </a>
                   </div>
-                  <div className='row mt-5'>
-                    <div className='signup_instead'>
+                  <div className='row mt-5 container'>
+                    <div className='signup_instead col-12 col-sm-6'>
                     <a href='/signup' className='login_links'>
                       Sign-Up Instead?
                     </a>
                     </div>
-                    <div className='login_button'>
-                      <Button color='primary' onClick={this.handleLogin.bind(this)}>Login</Button>
+                    <div className='login_button offset-sm-2 col-sm-4'>
+                    <Button variant="contained" color="primary" onClick = {handleLogin.bind(this)}>
+                      Log In
+                    </Button>
                     </div>
                   </div>
                 </div>
@@ -93,6 +82,6 @@ handleLogin() {
     </div>
       );
   }
-}
+
 
 export default Login;
